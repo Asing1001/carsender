f45e1ce1695e00c59b89a29fc9d9f88544c22d8c
Andy Chen <shinningstar1001@gmail.com>
2018-06-15 01:10:20 +0800

    first commit

---
+++ layouts/centered.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app id="inspire">
+    <v-content>
+      <v-container fluid fill-height>
+        <nuxt /> 
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      drawer: null
+    }),
+    props: {
+      source: String
+    }
+  }
+</script>
