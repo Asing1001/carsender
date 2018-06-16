8590c6aad905587a63598d0b2120ac1e9d66df55
Andy Chen <shinningstar1001@gmail.com>
2018-06-17 03:51:22 +0800

    update new-order form

--- pages/index.vue
+++ pages/index.vue
@@ -3,42 +3,41 @@
     <v-flex xs12 sm8 md6>
       <form>
         <v-select
-          v-model="select"
+          v-model="serviceType"
           :items="items"
-          :error-messages="selectErrors"
-          label="服務類型"
+          :error-messages="serviceTypeErrors"
+          label="預約類型"
           required
-          @change="$v.select.$touch()"
-          @blur="$v.select.$touch()"
+          @change="$v.serviceType.$touch()"
+          @blur="$v.serviceType.$touch()"
         ></v-select>
         <v-menu
-        ref="dateMenu"
-        :close-on-content-click="false"
-        v-model="dateMenu"
-        :nudge-right="40"
-        :return-value.sync="date"
-        lazy
-        transition="scale-transition"
-        offset-y
-        full-width
-        min-width="290px"
-      >
-        <v-text-field
-          slot="activator"
-          v-model="date"
-          label="乘車日期"
-          append-icon="event"
-          readonly
-        ></v-text-field>
-        <v-date-picker v-model="date" @input="$refs.dateMenu.save(date)"></v-date-picker>
-
+          ref="dateMenu"
+          :close-on-content-click="false"
+          v-model="dateMenu"
+          :nudge-right="40"
+          :return-value.sync="pickUpDate"
+          lazy
+          transition="scale-transition"
+          offset-y
+          full-width
+          min-width="290px"
+        >
+          <v-text-field
+            slot="activator"
+            v-model="pickUpDate"
+            label="乘車日期*"
+            append-icon="event"
+            readonly
+          ></v-text-field>
+        <v-date-picker v-model="pickUpDate" @input="$refs.dateMenu.save(pickUpDate)"></v-date-picker>
       </v-menu>
       <v-menu
         ref="menu"
         :close-on-content-click="false"
         v-model="timeMenu"
         :nudge-right="40"
-        :return-value.sync="time"
+        :return-value.sync="pickUpTime"
         lazy
         transition="scale-transition"
         offset-y
@@ -48,30 +47,30 @@
       >
         <v-text-field
           slot="activator"
-          v-model="time"
-          label="乘車時間"
+          v-model="pickUpTime"
+          label="乘車時間*"
           append-icon="access_time"
           readonly
         ></v-text-field>
-        <v-time-picker v-model="time" format="24hr" @change="$refs.menu.save(time)"></v-time-picker>
+        <v-time-picker v-model="pickUpTime" format="24hr" @change="$refs.menu.save(pickUpTime)"></v-time-picker>
       </v-menu>
       <v-text-field
-          v-model="pickUpPlace"
-          :error-messages="pickUpPlaceErrors"
+          v-model="pickUpAddress"
+          :error-messages="pickUpAddressErrors"
           :counter="10"
           label="乘車地址"
           required
-          @input="$v.pickUpPlace.$touch()"
-          @blur="$v.pickUpPlace.$touch()"
+          @input="$v.pickUpAddress.$touch()"
+          @blur="$v.pickUpAddress.$touch()"
         ></v-text-field>
         <v-text-field
-          v-model="targetPlace"
-          :error-messages="targetPlaceErrors"
+          v-model="targetAddress"
+          :error-messages="targetAddressErrors"
           :counter="10"
-          label="目的地"
+          label="目的地址"
           required
-          @input="$v.targetPlace.$touch()"
-          @blur="$v.targetPlace.$touch()"
+          @input="$v.targetAddress.$touch()"
+          @blur="$v.targetAddress.$touch()"
         ></v-text-field>
         <v-text-field
           v-model="name"
@@ -119,35 +118,35 @@
     validations: {
       name: { required },
       email: { email },
-      select: { required },
+      serviceType: { required },
       phone: { required },
       totalPeople: { required },
-      pickUpPlace: { required },
-      targetPlace: { required }
+      pickUpAddress: { required },
+      targetAddress: { required }
     },
     data: () => ({
-      select: null,
-      date: null,
+      serviceType: null,
+      pickUpDate: null,
       dateMenu: false,
-      time: null,
+      pickUpTime: null,
       timeMenu: false,
       name: '',
       email: '',
       phone: null,
       totalPeople: 1,
-      pickUpPlace: '',
-      targetPlace: '',
+      pickUpAddress: '',
+      targetAddress: '',
       items: [
-        '送機 (雙北=>桃園機場)',
-        '接機 (桃園機場=>雙北)',
+        '送機 (雙北 => 桃園機場)',
+        '接機 (桃園機場 => 雙北)',
         '包車 (限同行政區)'
       ]
     }),
     computed: {
-      selectErrors () {
+      serviceTypeErrors () {
         const errors = []
-        if (!this.$v.select.$dirty) return errors
-        !this.$v.select.required && errors.push('必填欄位')
+        if (!this.$v.serviceType.$dirty) return errors
+        !this.$v.serviceType.required && errors.push('必填欄位')
         return errors
       },
       nameErrors () {
@@ -156,16 +155,16 @@
         !this.$v.name.required && errors.push('必填欄位')
         return errors
       },
-      pickUpPlaceErrors () {
+      pickUpAddressErrors () {
         const errors = []
-        if (!this.$v.pickUpPlace.$dirty) return errors
-        !this.$v.pickUpPlace.required && errors.push('必填欄位')
+        if (!this.$v.pickUpAddress.$dirty) return errors
+        !this.$v.pickUpAddress.required && errors.push('必填欄位')
         return errors
       },
-      targetPlaceErrors () {
+      targetAddressErrors () {
         const errors = []
-        if (!this.$v.targetPlace.$dirty) return errors
-        !this.$v.targetPlace.required && errors.push('必填欄位')
+        if (!this.$v.targetAddress.$dirty) return errors
+        !this.$v.targetAddress.required && errors.push('必填欄位')
         return errors
       },
       emailErrors () {
