a62c725516289acb7976cdd8845f57a0291e8644
Andy Chen <shinningstar1001@gmail.com>
2018-06-17 17:26:46 +0800

    add mongoose api

--- api/index.js
+++ api/index.js
@@ -1,4 +1,9 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGODB_URI).then(
+  connection => console.log('[MongoDB Connection] success'),
+  console.error.bind(console, '[MongoDB Connection] error:'))
 
 // Create express router
 const router = express.Router()
@@ -20,13 +25,89 @@ router.post('/login', (req, res) => {
     req.session.authUser = { username: 'allen155475' }
     return res.json({ username: 'allen155475' })
   }
-  res.status(401).json({ message: 'Bad credentials' })
+  res.status(401).json({ message: '無效的帳號密碼' })
 })
 
 // Add POST - /api/logout
 router.post('/logout', (req, res) => {
   delete req.session.authUser
   res.json({ ok: true })
+})
+
+const orderSchema = new mongoose.Schema({
+  serviceType: {
+    type: String,
+    required: true,
+    maxlength: 50
+  },
+  pickUpDate: {
+    type: String,
+    required: true,
+    maxlength: 20
+  },
+  pickUpTime: {
+    type: String,
+    required: true,
+    maxlength: 20
+  },
+  pickUpAddress: {
+    type: String,
+    required: true,
+    maxlength: 200
+  },
+  targetAddress: {
+    type: String,
+    required: true,
+    maxlength: 200
+  },
+  name: {
+    type: String,
+    required: true,
+    maxlength: 200
+  },
+  phone: {
+    type: String,
+    required: true,
+    maxlength: 30
+  },
+  totalPeople: {
+    type: Number,
+    required: true,
+    max: 200,
+    min: 1
+  },
+  remark: {
+    type: String,
+    maxlength: 200
+  }
+})
+
+const Order = mongoose.model('Order', orderSchema)
+
+router.post('/orders', (req, res) => {
+  Order.create(req.body, (err, order) => {
+    if (err) {
+      res.status(400).json({message: err})
+    } else {
+      res.json({ ok: true, order })
+    }
+  })
+})
+
+router.get('/orders', (req, res) => {
+  Order.find({}, (err, orders) => {
+    if (err) {
+      res.status(400).json({message: err})
+    } else {
+      res.json({ ok: true, orders })
+    }
+  })
+})
+
+router.use((req, res, next) => {
+  console.log(req.body)
+  console.log(res)
+  next()
 })
 
 // Export the server middleware
