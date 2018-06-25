f4a5b7905a2548cc7f8a66241cc3e152b8af0187
Sing <sing.chen@linecorp.com>
2018-06-25 15:06:25 +0800

    redirect to login page if not authenticated

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,8 +1,9 @@
-export default function ({ store, error }) {
+export default function ({ store, error, route, redirect }) {
   if (!store.state.authUser) {
-    error({
-      message: '請登入以繼續',
-      statusCode: 403
-    })
+    // error({
+    //   message: '請登入以繼續',
+    //   statusCode: 403
+    // })
+    redirect('/login')
   }
 }
