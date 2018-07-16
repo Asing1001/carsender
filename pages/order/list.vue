32fc80167cc38649435d526ed888194d0021cc87
Andy Chen <shinningstar1001@gmail.com>
2018-07-17 01:13:37 +0800

    adjust style, add planeNo

--- pages/order/list.vue
+++ pages/order/list.vue
@@ -24,15 +24,16 @@
                 :rows-per-page-items="[10, 20, 50, 100]"
                 >
                 <template slot="items" slot-scope="{ item }">
-                    <td class="text-xs-right px-2">{{ item.serviceType }}</td>
-                    <td class="text-xs-right px-2">{{ item.pickUpDate }}</td>
-                    <td class="text-xs-right px-2">{{ item.pickUpTime }}</td>
-                    <td class="text-xs-right px-2">{{ item.pickUpCity + item.pickUpArea + item.pickUpAddress }}</td>
-                    <td class="text-xs-right px-2">{{ item.targetCity + item.targetArea + item.targetAddress }}</td>
-                    <td class="text-xs-right px-2">{{ item.name }}</td>
-                    <td class="text-xs-right px-2">{{ item.phone }}</td>
-                    <td class="text-xs-right px-2">{{ item.totalPeople }}</td>
-                    <td class="text-xs-right px-2">{{ item.remark }}</td>
+                    <td class="text-xs-left px-2">{{ item.serviceType }}</td>
+                    <td class="text-xs-left px-2">{{ item.planeNo }}</td>
+                    <td class="text-xs-left px-2 nowrap">{{ item.pickUpDate }}</td>
+                    <td class="text-xs-left px-2">{{ item.pickUpTime }}</td>
+                    <td class="text-xs-left px-2">{{ item.pickUpCity + item.pickUpArea + item.pickUpAddress }}</td>
+                    <td class="text-xs-left px-2">{{ item.targetCity + item.targetArea + item.targetAddress }}</td>
+                    <td class="text-xs-left px-2">{{ item.name }}</td>
+                    <td class="text-xs-left px-2">{{ item.phone }}</td>
+                    <td class="text-xs-left px-2">{{ item.totalPeople }}</td>
+                    <td class="text-xs-left px-2">{{ item.remark }}</td>
                     <td class="justify layout px-0">
                       <!-- <v-btn icon class="mx-0" @click="editItem(item)">
                         <v-icon color="teal">edit</v-icon>
@@ -64,20 +65,22 @@ export default {
       dialog: false,
       search: '',
       headers: [
-        { text: '預約類型', value: 'serviceType', class: 'text-xs-right px-2' },
-        { text: '乘車日期', value: 'pickUpDate', class: 'text-xs-right px-2' },
-        { text: '乘車時間', value: 'pickUpTime', class: 'text-xs-right px-2' },
-        { text: '乘車地址', value: 'pickUpAddress', class: 'text-xs-right px-2' },
-        { text: '目的地', value: 'targetAddress', class: 'text-xs-right px-2' },
-        { text: '姓名', value: 'name', class: 'text-xs-right px-2' },
-        { text: '手機', value: 'phone', class: 'text-xs-right px-2' },
-        { text: '人數', value: 'totalPeople', class: 'text-xs-right px-2' },
-        { text: '備註', value: 'remark', class: 'text-xs-right px-2' },
+        { text: '預約類型', value: 'serviceType', class: 'text-xs-left px-2' },
+        { text: '航班編號', value: 'planeNo', class: 'text-xs-left px-2' },
+        { text: '乘車日期', value: 'pickUpDate', class: 'text-xs-left px-2' },
+        { text: '時間', value: 'pickUpTime', class: 'text-xs-left px-2' },
+        { text: '乘車地址', value: 'pickUpAddress', class: 'text-xs-left px-2' },
+        { text: '目的地', value: 'targetAddress', class: 'text-xs-left px-2' },
+        { text: '姓名', value: 'name', class: 'text-xs-left px-2' },
+        { text: '手機', value: 'phone', class: 'text-xs-left px-2' },
+        { text: '人數', value: 'totalPeople', class: 'text-xs-left px-2' },
+        { text: '備註', value: 'remark', class: 'text-xs-left px-2' },
         { text: '', value: 'action', sortable: false }
       ],
       editedIndex: -1,
       editedItem: {
         serviceType: null,
+        planeNo: '',
         pickUpDate: null,
         pickUpTime: null,
         name: '',
@@ -90,6 +93,7 @@ export default {
       },
       defaultItem: {
         serviceType: null,
+        planeNo: '',
         pickUpDate: null,
         pickUpTime: null,
         name: '',
