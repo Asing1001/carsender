c69494e9478405df4e5db6a4680d5e79d414db46
Andy Chen <shinningstar1001@gmail.com>
2019-03-13 23:10:22 +0800

    remove unreachable cities

--- assets/cities.js
+++ assets/cities.js
@@ -6,22 +6,32 @@ const cities = [{
     { 'areaName': '松山區', 'zipCode': '105' },
     { 'areaName': '大安區', 'zipCode': '106' },
     { 'areaName': '萬華區', 'zipCode': '108' },
-    { 'areaName': '信義區', 'zipCode': '110' }, { 'areaName': '士林區', 'zipCode': '111' }, { 'areaName': '北投區', 'zipCode': '112' }, { 'areaName': '內湖區', 'zipCode': '114' }, { 'areaName': '南港區', 'zipCode': '115' }, { 'areaName': '文山區', 'zipCode': '116' }]
+    { 'areaName': '信義區', 'zipCode': '110' }, { 'areaName': '士林區', 'zipCode': '111' },
+    { 'areaName': '北投區', 'zipCode': '112' }, { 'areaName': '內湖區', 'zipCode': '114' },
+    { 'areaName': '南港區', 'zipCode': '115' }, { 'areaName': '文山區', 'zipCode': '116' }]
 }, { 'cityName': '新北市',
   'areas': [
     // { 'areaName': '萬里區', 'zipCode': '207' },
     // { 'areaName': '金山區', 'zipCode': '208' },
     { 'areaName': '板橋區', 'zipCode': '220' }, { 'areaName': '汐止區', 'zipCode': '221' },
     // { 'areaName': '深坑區', 'zipCode': '222' },
-    // { 'areaName': '石碇區', 'zipCode': '223' }, { 'areaName': '瑞芳區', 'zipCode': '224' },
+    // { 'areaName': '石碇區', 'zipCode': '223' },
+    // { 'areaName': '瑞芳區', 'zipCode': '224' },
     // { 'areaName': '平溪區', 'zipCode': '226' },
-    // { 'areaName': '雙溪區', 'zipCode': '227' }, { 'areaName': '貢寮區', 'zipCode': '228' },
+    // { 'areaName': '雙溪區', 'zipCode': '227' },
+    // { 'areaName': '貢寮區', 'zipCode': '228' },
     { 'areaName': '新店區', 'zipCode': '231' },
     // { 'areaName': '坪林區', 'zipCode': '232' },
-    { 'areaName': '烏來區', 'zipCode': '233' },
+    // { 'areaName': '烏來區', 'zipCode': '233' },
     { 'areaName': '永和區', 'zipCode': '234' },
     { 'areaName': '中和區', 'zipCode': '235' }, { 'areaName': '土城區', 'zipCode': '236' },
     { 'areaName': '三峽區', 'zipCode': '237' }, { 'areaName': '樹林區', 'zipCode': '238' },
     { 'areaName': '鶯歌區', 'zipCode': '239' }, { 'areaName': '三重區', 'zipCode': '241' },
-    { 'areaName': '新莊區', 'zipCode': '242' }, { 'areaName': '泰山區', 'zipCode': '243' }, { 'areaName': '林口區', 'zipCode': '244' }, { 'areaName': '蘆洲區', 'zipCode': '247' }, { 'areaName': '五股區', 'zipCode': '248' }, { 'areaName': '八里區', 'zipCode': '249' }, { 'areaName': '淡水區', 'zipCode': '251' }, { 'areaName': '三芝區', 'zipCode': '252' }, { 'areaName': '石門區', 'zipCode': '253' }] }]
+    { 'areaName': '新莊區', 'zipCode': '242' }, { 'areaName': '泰山區', 'zipCode': '243' },
+    { 'areaName': '林口區', 'zipCode': '244' }, { 'areaName': '蘆洲區', 'zipCode': '247' },
+    { 'areaName': '五股區', 'zipCode': '248' }, { 'areaName': '八里區', 'zipCode': '249' }
+    // { 'areaName': '淡水區', 'zipCode': '251' },
+    // { 'areaName': '三芝區', 'zipCode': '252' },
+    // { 'areaName': '石門區', 'zipCode': '253' }
+  ] }]
 export default cities
