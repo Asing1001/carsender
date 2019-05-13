0d279697efb05834b90997a906e381340d6a68e3
luluchen1022 <luluchen1022@gmail.com>
2019-05-14 00:24:31 +0800

    request temp

--- api/pay.js
+++ api/pay.js
@@ -1,6 +1,9 @@
-const axios = require('axios')
 const url = require('url')
-const fixieUrl = url.parse(process.env.FIXIE_URL || 'fixie:bYoRgDvl5AbYgZi@speedway.usefixie.com:1080')
+const axios = require('axios')
+const fixieUrl = url.parse(
+  process.env.FIXIE_URL ||
+    'http://fixie:0lgWxt8mqAbJRct@velodrome.usefixie.com:80'
+)
 
 const isSandbox = !process.env.LINE_PAY_CHANNEL_ID
 const linePayAPI = isSandbox
@@ -10,8 +13,15 @@ const payHeaders = {
   'X-LINE-ChannelId': process.env.LINE_PAY_CHANNEL_ID || '1649897844',
   'X-LINE-ChannelSecret':
     process.env.LINE_PAY_CHANNEL_SECRET || '9c0a1cef572a6f498467b99f801dc68f',
-  'Content-Type': 'application/json',
+  'Content-Type': 'application/json'
 }
+const request = require('request')
+const fixieRequest = request.defaults({
+  proxy: fixieUrl,
+  headers: payHeaders,
+  baseUrl: linePayAPI,
+  timeout: 10000
+})
 
 const client = axios.create({
   baseURL: linePayAPI,
@@ -22,7 +32,7 @@ const client = axios.create({
     host: fixieUrl.hostname,
     port: fixieUrl.port,
     auth: fixieUrl.auth
-  },
+  }
 })
 
 async function reserve({ order, confirmUrl }) {
@@ -35,8 +45,23 @@ async function reserve({ order, confirmUrl }) {
     orderId: order._id
     // confirmUrlType: 'SERVER' // to indicate its web
   }
-  const { data } = await client.post('payments/request', linePayOrder)
-  return data
+  return new Promise(resolve => {
+    fixieRequest.post(
+      'payments/request',
+      { json: linePayOrder },
+      (error, res, body) => {
+        if (error) {
+          console.error(error)
+          return
+        }
+        console.log(`statusCode: ${res.statusCode}`)
+        console.log(body)
+        resolve(body)
+      }
+    )
+  })
+  // const { data } = await client.post('payments/request', linePayOrder)
+  return res
 }
 
 async function confirm(options) {
